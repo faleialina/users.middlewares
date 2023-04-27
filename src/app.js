5c8a28f41d435a496b73697fe182855fd80ba77e
faleialina <august3@mail.ru>
2023-04-27 23:33:38 +0300

    users.middlewares/middleware.err

--- src/app.js
+++ src/app.js
@@ -9,5 +9,10 @@ app.use(bodyParser.json());
 
 app.use('/user', router);
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 
 module.exports = { app };
