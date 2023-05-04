ae83ce21a6db3a5f1e995a5b1e027ddb3a3de260
faleialina <august3@mail.ru>
2023-05-04 23:13:21 +0300

    form

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const user = require('./controller/user.controller');
+
+const app = express();
 
 app.use(bodyParser.json())
 
